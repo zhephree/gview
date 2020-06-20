f4c1254e30f9c551d4a5fa589b99dc6ceaf23e8d
Geoffrey Gauchet <geoff@zhephree.com>
2020-06-20 01:11:40 -0500

    support for nav; requests

--- test/controllers/hello.js
+++ test/controllers/hello.js
@@ -11,6 +11,41 @@ class Hello extends Controller {
 	something(){
 		alert('YESSSS!');
 	}
+
+	getTest(){
+		Request.get('http://dummy.restapiexample.com/api/v1/employees')
+			.then((r) => {
+				console.log(r);
+			})
+	}
+
+	postTest(){
+		Request.post('http://dummy.restapiexample.com/api/v1/create',
+			{
+				data: {
+					name: "Geoff",
+					salary: "123456",
+					age: "36"
+				}
+			})
+			.then((r) => {
+				console.log(r);
+			})
+	}
+
+	putTest(){
+		Request.put('http://dummy.restapiexample.com/api/v1/update/21',
+			{
+				data: {
+					name: "Geoff",
+					salary: "123456",
+					age: "36"
+				}
+			})
+			.then((r) => {
+				console.log(r);
+			})
+	}
 }
 
 App.use(Hello);
